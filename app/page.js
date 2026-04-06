441c8471cf8bfa19b02a80633bf8b218af0d63cf
idwink37 <idwink37@gmail.com>
2026-04-06 13:07:23 +0900

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>📦 창고관리</h2>
+
+      <Link href="/stock">재고</Link> | <Link href="/list">기록</Link>
+
+      <p>정상 작동 🚀</p>
+    </div>
+  );
+}
