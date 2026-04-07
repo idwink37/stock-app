2d8e425875cfd67205b30091a0379b00977d1071
idwink37 <idwink37@gmail.com>
2026-04-08 08:53:10 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,9 +7,8 @@ export default function Home() {
     <div style={{ padding: 20 }}>
       <h2>📦 창고관리</h2>
 
-      <Link href="/stock">재고</Link> | <Link href="/list">기록</Link>
-
-      <p>정상 작동 🚀</p>
+      <Link href="/stock">재고</Link> |{" "}
+      <Link href="/list">기록</Link>
     </div>
   );
 }
